afba9dd2ea146bc7d2e8dac2b039935136ce3fe3
sebastianhonorat-dev <youremail@example.com>
2026-06-11 21:13:44 -0400

    added dstar algo

--- 8_puzzle.py
+++ 8_puzzle.py
@@ -1,103 +1,116 @@
-import random
+from queue import Queue
+import math
 
-__goal = [1,2,3,
-          4,5,6,
-          7,8,9]
+def BFS_Shortest_Path(Start, Goal):
+    Q = Queue()
+    visited = set()
+    Q.put((Start, 0))
+    visited.add(Start)
+    min_steps = math.inf
 
-class Node:
+    while not Q.empty():
+        (board, steps) = Q.get()
+        blank_pos = board.index(0)
+        row = blank_pos // 3
+        column = blank_pos % 3
+        if board == Goal:
+            return steps
 
-    def __init__(self, value, prev=None):
-        self.value=value
-        self.prev=None
+        for dx, dy in [(0,1), (0,-1),(1,0),(-1,0)]:
+            new_row = row + dx
+            new_column = column + dy
 
-        
+            if new_row >= 0 and new_row <= 2 and new_column >= 0 and new_column <=2:
+                swap_pos = new_row * 3 + new_column
+                temp_board = list(board)
+                temp_board[blank_pos] = temp_board[swap_pos]
+                temp_board[swap_pos] = 0
+                new_board = tuple(temp_board)
 
-    def display_cube(self,puzzle:tuple):
-        # print(puzzle)
-        if not puzzle:
-            print(puzzle)
-        else:
-            for i in range(3):
-                start=i*3
-                end=(i+1)*3
-                print(puzzle[start:end])
-        print()
+                if new_board not in visited:
+                    visited.add(new_board)
+                    Q.put((new_board, steps + 1))
+    return -1
 
-    def randomize_face(self):
-        pieces = list(self.value)
-        face = ()
-
-        while pieces:
-
-            index=random.randint(0,len(pieces)-1)
-            piece = pieces.pop(index)
-
-            face +=(piece,)
-        self.display_cube(face)
-        self.value=face
-        return face
+x = BFS_Shortest_Path((0,1,2,3,4,5,6,7,8),(1,2,0,3,4,5,6,7,8))
+print(f"Answer is {x}")
 
 
-    def get_moves(self,puzzle:tuple):
-        puzzle=list(puzzle)
-        possible_moves = []
+def Dijkstra(Start, Goal):
+    Q = []
+    visited = dict()
 
-        blank=9
-        blank_index = puzzle.index(blank)
+    def pull_from_heap(Q):
+        minimum=Q[0]
 
-        if not puzzle:
-            return
-        
-        if blank_index not in [0,3,6]:
-            temp_puzzle=puzzle.copy()
-            temp_puzzle[blank_index]=temp_puzzle[blank_index-1]
-            temp_puzzle[blank_index-1]=blank
-            possible_moves.append(temp_puzzle)
+        last_index = len(Q)-1
+        Q[0]=Q[last_index]
+        Q.pop()
 
+        current_index=0
 
-        if blank_index not in [2,5,8] :
-            temp_puzzle=puzzle.copy()
-            temp_puzzle[blank_index]=temp_puzzle[blank_index+1]
-            temp_puzzle[blank_index+1]=blank
-            possible_moves.append(temp_puzzle)
-
-
-        if blank_index < 6: 
-            temp_puzzle=puzzle.copy()
-            temp_puzzle[blank_index]=temp_puzzle[blank_index+3]
-            temp_puzzle[blank_index+3]=blank
-            possible_moves.append(temp_puzzle)
-
-        if blank_index >= 3:
-            temp_puzzle=puzzle.copy()
-            temp_puzzle[blank_index]=temp_puzzle[blank_index-3]
-            temp_puzzle[blank_index-3]=blank
-            possible_moves.append(temp_puzzle)
-
-        # print(possible_moves)
-        possible_nodes=[]
-        for move in possible_moves:
-            self.display_cube(move)
-            node = Node(move)
+        if len(Q) > 2:
             
-            possible_nodes.append(node)
-        return(possible_nodes)
+            while 2 * current_index + 1 < len(Q):
+                left_index = 2 * current_index + 1
+                left = Q[left_index]
+
+                if steps > left[1]:
+                    temp = Q[current_index]
+                    Q[current_index] = Q[left_index]
+                    Q[left_index] = temp
+                    current_index = left_index
     
-    
-    def is_goal(self, move:list):
-        if move == __goal:
-            return True
-        else:
-            return False
+        return minimum
 
 
-root=Node([1,2,3,4,5,6,7,9,8])
-root.randomize_face()
-# print("Start\n",root.value,"\n")
-moves_list=root.get_moves(root.value)
-# print(moves)
-# root.set_moves(moves)
-for move in moves_list:
-    child = Node(move,root)
+    def push_to_heap(board, steps):
+        Q.append((board, steps))
+        current_index=len(Q)-1
 
+        while True:
+            parent_index = (current_index - 1) // 2
+            parent = Q[parent_index]
 
+            if steps < parent[1]:
+                temp = Q[current_index]
+                Q[current_index] = Q[parent_index]
+                Q[parent_index] = temp
+                current_index = parent_index
+
+            else:
+                return None
+            
+    push_to_heap(Start, 0)
+    visited[Start]=0
+
+    while Q:
+        (board, steps) = pull_from_heap(Q)
+        blank_pos = board.index(0)
+        row = blank_pos // 3
+        column = blank_pos % 3
+        if board == Goal:
+            return steps
+
+        for dx, dy in [(0,1), (0,-1),(1,0),(-1,0)]:
+            new_row = row + dx
+            new_column = column + dy
+
+            if new_row >= 0 and new_row <= 2 and new_column >= 0 and new_column <=2:
+                swap_pos = new_row * 3 + new_column
+                temp_board = list(board)
+                temp_board[blank_pos] = temp_board[swap_pos]
+                temp_board[swap_pos] = 0
+                new_board = tuple(temp_board)
+                steps += 1
+
+                if new_board not in visited:
+                    visited[new_board]=steps
+                    push_to_heap(new_board, steps)
+
+                if visited[new_board] > steps:
+                    visited[new_board]=steps
+    return -1
+
+x = Dijkstra((0,1,2,3,4,5,6,7,8),(1,2,0,3,4,5,6,7,8))
+print(f"Answer is {x}")
